761410e6af19d18e4db92b80232b0e900d814221
Alvaro Garcia <alvaro.garcia@grupomok.com>
2023-02-01 17:43:52 -0300

    initial commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const h1 = document.querySelector('h1')
+const p = document.querySelector('p')
+const paragraph = document.querySelector('.paragraph')
+const pid = document.querySelector('#third')
+const input = document.querySelector('input')
+
+console.log({
+    h1, p, paragraph, pid, input
+})
